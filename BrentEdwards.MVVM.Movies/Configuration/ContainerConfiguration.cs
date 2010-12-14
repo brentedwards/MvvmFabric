15370b56710c716b5f72eec5928b58b136ce889b
brentedwards <brent.l.edwards@gmail.com>
2010-12-14 00:56:29 -0600

    Adding project for Castle Windsor implementation.

--- BrentEdwards.MVVM.Movies/Configuration/ContainerConfiguration.cs
+++ BrentEdwards.MVVM.Movies/Configuration/ContainerConfiguration.cs
@@ -10,6 +10,7 @@ using BrentEdwards.MVVM.Navigation;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using BrentEdwards.MVVM.Movies.Client.Views;
+using BrentEdwards.MVVM.Castle.Windsor;
 
 namespace Movies.Client.Configuration
 {
@@ -37,48 +38,16 @@ namespace Movies.Client.Configuration
 					.ImplementedBy<ViewFactory>()
 					.LifeStyle.Singleton);
 
-			InitNavigation();
+			var viewConfigResolver = new WindsorViewConfigurationResolver(container);
+			container.Register(Component.For<IViewConfigurationResolver>()
+				.Instance(viewConfigResolver));
+
+			InitNavigation(viewConfigResolver);
 		}
 
-		private static void InitNavigation()
+		private static void InitNavigation(IViewConfigurationResolver viewConfigResolver)
 		{
-			RegisterViewConfiguration<Detail>(MoviesViewTargets.Detail);
-		}
-
-
-		private static void RegisterViewConfiguration<TView>(ViewTargets viewTarget) where TView : FrameworkElement
-		{
-			// If a view is named "Blah", register the ViewConfiguration as "Blah",
-			// the view as "BlahView", and the view model (if it needs to be explicitly
-			// defined) as "BlahViewModel".
-			ComponentContainer.Container.Register(
-				Component.For<FrameworkElement>()
-					.Named(String.Format("{0}View", viewTarget.ToString()))
-					.ImplementedBy<TView>()
-					.LifeStyle.Transient,
-				Component.For<ViewConfiguration>()
-					.Named(viewTarget.ToString())
-					.LifeStyle.Transient
-					.Parameters(Parameter.ForKey("view").Eq(String.Format("${{{0}View}}", viewTarget.ToString()))));
-		}
-
-		private static void RegisterViewConfiguration<TView, TViewModel>(ViewTargets viewTarget) where TView : FrameworkElement
-		{
-			ComponentContainer.Container.Register(
-				Component.For<Object>()
-					.Named(String.Format("{0}ViewModel", viewTarget.ToString()))
-					.ImplementedBy<TViewModel>()
-					.LifeStyle.Transient,
-				Component.For<FrameworkElement>()
-					.Named(String.Format("{0}View", viewTarget.ToString()))
-					.ImplementedBy<TView>()
-					.LifeStyle.Transient,
-				Component.For<ViewConfiguration>()
-					.Named(viewTarget.ToString())
-					.LifeStyle.Transient
-					.Parameters(
-						Parameter.ForKey("view").Eq(String.Format("${{{0}View}}", viewTarget.ToString())),
-						Parameter.ForKey("viewModel").Eq(String.Format("${{{0}ViewModel}}", viewTarget.ToString()))));
+			viewConfigResolver.RegisterViewConfiguration<Detail>(MoviesViewTargets.Detail);
 		}
 	}
 }
